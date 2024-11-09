196da9e3f707aaa09da6e0cba46de2a5f764a697
Andree Figueroa <asfo.2009@gmail.com>
2024-11-08 21:42:19 -0300

    test: enhance AuthController tests with mock AuthService and add SignIn test case

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    const mockAuthService = {
+      SignIn: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('should Sign In', async () => {
+    const result = {
+      access_token: expect.any(String),
+    };
+    const signInDto = {
+      email: 'email.com',
+      password: 'password',
+    };
+
+    jest.spyOn(authService, 'SignIn').mockResolvedValue(result);
+
+    expect(await controller.signIn(signInDto)).toBe(result);
   });
 });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { SignInDto } from './dto/sign-in.dto';
